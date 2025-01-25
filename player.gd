7eaa8c95e0cf099d0234460c0de1dfe2d4edf053
Valtteri <valtteri.nietula@gmail.com>
2025-01-25 12:52:32 +0200

    Added tweeting

--- player.gd
+++ player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 var targetPosition: Vector2 = position
 const speed: int = 25
 
+@export var tweetButton: Button
+
+func _ready():
+	tweetButton.tweet_recieved.connect(_on_tweet_recieved)
+	
 
 func _on_button_send_impulse(impulseVector:Vector2) -> void:
 	targetPosition = position + impulseVector
@@ -18,5 +23,7 @@ func _process(delta: float) -> void:
 		position = targetPosition
 	
 	
+func _on_tweet_recieved(result):
+	targetPosition = position+Vector2(0,100)
 func _draw():
 	draw_circle((targetPosition-global_position), 10, Color("BLUE"))
---
+++ tweet.gd
@@ -0,0 +1,15 @@
+extends Button
+
+signal tweet_recieved(result)
+
+@export var tweetText: TextEdit
+@export var API: Node
+
+
+
+
+func _on_pressed() -> void:
+	var tweet = tweetText.text
+	var result = await API.get_similarity_async(tweet)
+	tweet_recieved.emit(result)
+	print("Tweet done")
