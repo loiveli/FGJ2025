3e58f1b1c404ead79f8af0230fe98b30d9145a8e
xizted <valtteri.nietula@gmail.com>
2025-01-26 01:48:13 +0200

    Follower logic

--- tweet.gd
+++ tweet.gd
@@ -31,11 +31,11 @@ func sendTweet(tweet):
 		bubblesSent= true
 		print("Tweet done")
 
-	var followers: int = 0
-	for sim in result:
+	var followers: int = player.followers*(tweet.length()/140+result[0].value)
+	for sim in result.slice(1):
 		print(sim.bubble," ",sim.value)
 		if sim.value > 0.3:
-			followers += (sim.value*followers) * tweet.length()*100
-	
+			followers += sim.value*player.followers*0.5
+			print("followers: ",(sim.value*followers)," ",tweet.length()," ",100)
 	player.followers += followers
 	
