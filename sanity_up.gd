cd3301da0fd3893ec5c930909f8862148ecc0e12
Valtteri <valtteri.nietula@gmail.com>
2025-01-26 13:34:59 +0200

    Added sanity up

--- sanity_up.gd
+++ sanity_up.gd
@@ -5,6 +5,8 @@ var collisionType = "MEME"
 var nodeList: Array
 	
 func _ready():
+    if not nodeList:
+        nodeList = $"..".get_children().filter(func(child): return child is StaticBody2D).filter(func(node): return node.collisionType == "BUBBLE")
     resetPosition(nodeList)
 	
 func resetPosition(nodes = nodeList):
@@ -12,8 +14,8 @@ func resetPosition(nodes = nodeList):
     var node1 = nodes.pick_random()
     nodes.remove_at( nodes.find(node1))
     var node2 = nodes.pick_random()
+    position = node2.position+((node1.position-node2.position) * randf_range(0.2,0.8))
 
-    position = ((node1.position) - node2.position) * randf_range(0.2,0.8)
 
 
 func initializeNodes(nodes):
